c49e114d0b12403eefb8935e3be4df305b1feb7a
oscartrevino-ti <44791985+oscartrevino-ti@users.noreply.github.com>
2022-09-06 16:27:21 -0500

    second commit

---
+++ src/view/ruby2d.rb
@@ -0,0 +1,42 @@
+require "ruby2d"
+
+module View
+
+    class Ruby2dView
+
+        def initialize
+            @pixel_size = 50
+        end
+
+        def render(state)
+        extend Ruby2d::DSL
+            set(
+                title: "Snake", 
+                width: @pixel_size * state.grid.cols, 
+                height: @pixel_size * state.grid.rows)
+
+                render_food(state)
+                render_snake(state)
+            show
+        end
+
+        def render_food(state)
+            extend Ruby2d::DSL
+            food = state.food
+            Square.new(
+                x: food.col * @pixel_size, 
+                y: food.row * @pixel_size,
+                size: @pixel_size,
+                color: 'blue'
+                
+            )
+        end
+
+        def render_snake(state)
+        end
+         
+     end
+
+end
+
+end
